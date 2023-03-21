623b9747bedd3260e5358f1015e68b30d5115997
Jakub <Wilk>
2023-03-21 09:25:08 -0500

    Initial Commit

---
+++ countrygamee/countrygameeApp.swift
@@ -0,0 +1,17 @@
+//
+//  countrygameeApp.swift
+//  countrygamee
+//
+//  Created by Jakub Wilk on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct countrygameeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
